ff9d822b675f039c8aa80ca59b9afc8815ed649d
hzc073 <759737361@qq.com>
2026-02-08 11:28:02 +0800

    v1.0.11 fix: #47 fix: #43 fix: #42

--- memos_flutter_app/lib/features/settings/laboratory_screen.dart
+++ memos_flutter_app/lib/features/settings/laboratory_screen.dart
@@ -117,7 +117,7 @@ class LaboratoryScreen extends ConsumerWidget {
                       ),
                       const SizedBox(height: 4),
                       Text(
-                        'VERSION 1.0.10',
+                        'VERSION 1.0.11',
                         style: TextStyle(
                           fontSize: 11,
                           fontWeight: FontWeight.w700,
